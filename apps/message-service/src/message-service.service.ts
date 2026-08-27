f83ef79b9b77c993de85085771280a1ee2ebfbd7
Erik Neves <erikpdn1@gmail.com>
2026-08-27 15:53:01 -0300

    feat: inicia a criação da message-service e das regras de negocio

--- apps/message-service/src/message-service.service.ts
+++ apps/message-service/src/message-service.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { MessageDocument } from './database/schema';
+import { MessageResponse, MessageStatus, SendMessageDto } from '@app/contracts';
 
 @Injectable()
 export class MessageServiceService {
   constructor(
-    @InjectModel('Message') private messageModel: Model<MessageDocument>,
+    @InjectModel('Message')
+    private readonly messageModel: Model<MessageDocument>,
+    private readonly conversationService: ChatService,
   ) {}
+
+  async sendMessage(
+    message: SendMessageDto,
+    senderId: string,
+  ): Promise<MessageResponse> {
+    if (!message.conversationId) {
+      throw new BadRequestException('conversationId is required');
+    }
+
+    const conversation = await this.conversationService.getConversationById(
+      message.conversationId,
+    );
+  }
 }
